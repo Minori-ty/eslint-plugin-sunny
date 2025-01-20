89e7c651f9f4002429938c8efe2c5fd96f800f42
天平 <1229248506@qq.com>
2025-01-21 02:15:16 +0800

    feat: 优化报错类型

--- src/rules/no-className-allowed.ts
+++ src/rules/no-className-allowed.ts
@@ -13,28 +13,32 @@ const rule: TSESLint.RuleModule<string, []> = {
         },
         fixable: 'code',
         messages: {
-            className: '在 page.{js,ts,jsx,tsx} 文件中不允许使用 className。',
+            js: '在 page.js 文件中不允许使用 className。',
+            ts: '在 page.ts 文件中不允许使用 className。',
+            jsx: '在 page.jsx 文件中不允许使用 className。',
+            tsx: '在 page.tsx 文件中不允许使用 className。',
         },
         schema: [],
     },
     create(context: TSESLint.RuleContext<string, []>): TSESLint.RuleListener {
-        const fileName = path.basename(context.getFilename())
+        const fileName = path.basename(context.filename)
+        const fileExt = path.extname(fileName).slice(1)
 
         // 检查文件名是否为 page.{js,ts,jsx,tsx}
-        if (fileName.match(/page\.(js|ts|jsx|tsx)$/)) {
-            return {
-                JSXAttribute(node) {
-                    if (node.name.name === 'className') {
-                        context.report({
-                            node,
-                            messageId: 'className',
-                        })
-                    }
-                },
-            }
+        if (!fileName.match(/page\.(js|ts|jsx|tsx)$/)) {
+            return {} // 如果不是 page.{js,ts,jsx,tsx}，返回空对象
         }
 
-        return {} // 如果不是 page.{js,ts,jsx,tsx}，返回空对象
+        return {
+            JSXAttribute(node) {
+                if (node.name.name === 'className') {
+                    context.report({
+                        node,
+                        messageId: fileExt,
+                    })
+                }
+            },
+        }
     },
     defaultOptions: [],
 }
